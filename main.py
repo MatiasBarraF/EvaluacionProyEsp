3b04c96551aa2a06582ba9c191730227cf63b245
MatiasBarraF <mrbarra@uc.cl>
2022-08-16 18:51:19 -0400

    update

---
+++ main.py
@@ -0,0 +1,12 @@
+import json
+ 
+# with open("farmers.json") as jsonFile:
+#     jsonObject = json.load(jsonFile)
+#     jsonFile.close()
+# print(jsonObject)
+
+if __name__ == "__main__":
+    print("funcion1")
+    print("funcion2")
+    print("funcion3")
+    
